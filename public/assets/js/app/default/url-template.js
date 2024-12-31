debb495f6dea36445f05aa78fd49f3c0c313a3f6
Aldair Trejo <147780739+aldairtrejot@users.noreply.github.com>
2024-12-31 11:59:55 -0600

    more changes

--- public/assets/js/app/default/url-template.js
+++ public/assets/js/app/default/url-template.js
@@ -7,6 +7,7 @@
  * 
  * URL_DEFAULT: / -> DOMINIO
  * 
+ * 1.- CREAR UN ARCHIVO NUEVO url.js, copiar el contenido de este en el archivo nuevo porque es el que se ejecuta
  */
 
 const URL_DEFAULT = ''; //URL_DE_CAMBIO_EN_LUGAR_DOMINIO
