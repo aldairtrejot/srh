6cdbbbadef9b6868b1c2ad56116a01e2fca2da4a
Aldair Trejo <147780739+aldairtrejot@users.noreply.github.com>
2025-01-20 18:24:07 -0600

    cambios en letter

--- public/assets/js/app/letter/letter/validate.js
+++ public/assets/js/app/letter/letter/validate.js
@@ -1,31 +1,95 @@
 // Validacion de fórmulario
+
 document.getElementById("myForm").addEventListener("submit", function (event) {
     let fecha_inicio = document.getElementById('fecha_inicio').value;
     let fecha_fin = document.getElementById('fecha_fin').value;
 
-    if (//Validacion de campos requeridos y max caracteres
+    if (
+        //isPositiveInteger($('#num_flojas').val(), 'No. hojas') ||
+        isFieldEmpty($('#num_documento').val(), 'No. Documento') ||
         isFieldEmpty($('#fecha_inicio').val(), 'Fecha de inicio') ||
         isFieldEmpty($('#fecha_fin').val(), 'Fecha fin') ||
+        isFieldEmpty($('#num_flojas').val(), 'No. hojas') ||
+        isFieldEmpty($('#folio_gestion').val(), 'Folio de gestión') ||
         isFieldEmpty($('#asunto').val(), 'Asunto') ||
-        isFieldEmpty($('#observaciones').val(), 'Observaciones') ||
-        isExceedingLength($('#asunto').val(), 'Asunto', 100) ||
-        isExceedingLength($('#observaciones').val(), 'Observaciones', 100)) {
+        isFieldEmpty($('#id_cat_area').val(), 'Área') ||
+        isFieldEmpty($('#id_usuario_area').val(), 'Usuario') ||
+        isFieldEmpty($('#id_usuario_enlace').val(), 'Enlace') ||
+        isFieldEmpty($('#id_cat_unidad').val(), 'Unidad') ||
+        isFieldEmpty($('#id_cat_coordinacion').val(), 'Coordinación') ||
+        isFieldEmpty($('#id_cat_estatus').val(), 'Estatus') ||
+        isFieldEmpty($('#id_cat_tramite').val(), 'Tramite') ||
+        isFieldEmpty($('#id_cat_clave').val(), 'Clave') ||
+        isFieldEmpty($('#puesto_remitente').val(), 'Puesto remitente') ||
+        isExceedingLength($('#folio_gestion').val(), 'Folio de gestión', 50) ||
+        isExceedingLength($('#puesto_remitente').val(), 'Puesto remitente', 100) ||
+        isExceedingLength($('#num_documento').val(), 'No. Documento', 50) ||
+        isExceedingLength($('#lugar').val(), 'Lugar', 250) ||
+        isExceedingLength($('#asunto').val(), 'Asunto', 130) ||
+        isExceedingLength($('#observaciones').val(), 'Observaciones', 130)) {
         event.preventDefault();  // Evita el envío del formulario
         return;  // Detener la ejecución aquí
     }
 
-    if ($('#es_por_area').val()) {//Validacion de check activo
-        if (isFieldEmpty($('#id_cat_area_documento').val(), 'Área') ||
-            isFieldEmpty($('#id_usuario_area_aux').val(), 'Usuario') ||
-            isFieldEmpty($('#id_usuario_enlace_aux').val(), 'Enlace')) { // valida que el campo este seleccionado
+    // Valida el check de agregar remitentes, mas de dos
+    if ($('#son_mas_remitentes').val()) {
+        if (isFieldEmpty($('#remitente').val(), 'Remitente') ||
+            isExceedingLength($('#remitente').val(), 'Remitente', 230)) {
             event.preventDefault();  // Evita el envío del formulario
             return;  // Detener la ejecución aquí
         }
-    } else {
-        let isValid = getNoCorrespondencia($('#num_correspondencia').val());
-        if (isValid) {
-            notyfEM.error('El No. Correspondencia no se encuentra asociado');
+    }
+
+    //Validacion para agregar remitente al sistema
+    if ($('#rfc_remitente_bool').val()) {
+        if (isFieldEmpty($('#remitente_nombre').val(), 'Nombre') ||
+            isFieldEmpty($('#remitente_apellido_paterno').val(), 'Apellido paterno') ||
+            isFieldEmpty($('#remitente_apellido_materno').val(), 'Unidad') ||
+            isExceedingLength($('#remitente_nombre').val(), 'Apellido materno', 50) ||
+            isExceedingLength($('#remitente_apellido_paterno').val(), 'Apellido paterno', 50) ||
+            isExceedingLength($('#remitente_apellido_materno').val(), 'Apellido materno', 50) ||
+            isExceedingLength($('#remitente_rfc').val(), 'RFC', 13)) {
+            event.preventDefault();  // Evita el envío del formulario
+            return;  // Detener la ejecución aquí
+        }
+
+        //Valida la estructura que el rfc sea correcta
+        if ($('#remitente_rfc').val() !== '') {
+            if (!validateRfc($('#remitente_rfc').val())) {
+                notyfEM.error('El RFC de remitente no es valido.');
+                event.preventDefault();  // Detener el envío si la validación falla
+                return;  // Detener la ejecución aquí
+            }
+        }
+
+        // Validacion de nombre unico de remitente
+        let isValidN = getUniqueRemitente($('#remitente_nombre').val(), 'nombre');
+        if (isValidN) {
+            notyfEM.error('El Nombre de remitente ya está registrado.');
             event.preventDefault();  // Detener el envío si la validación falla
+            return;  // Detener la ejecución aquí
+        }
+
+        // Validacion de rfc unico de remitente
+        let isValidR = getUniqueRemitente($('#remitente_rfc').val(), 'rfc');
+        console.log(isValidR);
+        if (isValidR) {
+            notyfEM.error('El RFC de remitente ya está registrado.');
+            event.preventDefault();  // Detener el envío si la validación falla
+            return;  // Detener la ejecución aquí
+        }
+
+    } else {
+        if (isFieldEmpty($('#id_cat_remitente').val(), 'Remitente')) {
+            event.preventDefault();  // Evita el envío del formulario
+            return;  // Detener la ejecución aquí
+        }
+    }
+
+    // Valida que si las fechas son iguales sean requeridas las horas
+    if (fecha_inicio == fecha_fin) {
+        if (isFieldEmpty($('#horas_respuesta').val(), 'Horas respuesta')) {
+            event.preventDefault();  // Evita el envío del formulario
             return;  // Detener la ejecución aquí
         }
     }
@@ -37,9 +101,23 @@ document.getElementById("myForm").addEventListener("submit", function (event) {
         return; // Detener la ejecución aquí
     }
 
-    $('#num_documento_area').prop('disabled', false); //desabilitar contenid
-});
+    // Validacion de no document sea unico
+    let isValid = getNoUnique($('#id_tbl_correspondencia').val(), $('#num_documento').val(), 'num_documento');
+    if (isValid) {
+        notyfEM.error('El No. Documento ya está registrado.');
+        event.preventDefault();  // Detener el envío si la validación falla
+        return;  // Detener la ejecución aquí
+    }
 
+    let isValidG = getNoUnique($('#id_tbl_correspondencia').val(), $('#folio_gestion').val(), 'folio_gestion');
+    if (isValidG) {
+        notyfEM.error('El Folio de gestión ya está registrado.');
+        event.preventDefault();  // Detener el envío si la validación falla
+        return;  // Detener la ejecución aquí
+    }
+
+    $('#id_cat_estatus').prop('disabled', false); //desabilitar contenido
+});
 
 //Validacion cuando se cambia el evento de fecha
 $('#fecha_inicio').change(function () {
