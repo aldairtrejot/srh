e3d40125a20eab586e32aebcd8dc4c74bb370855
Aldair Trejo <147780739+aldairtrejot@users.noreply.github.com>
2025-01-23 12:10:00 -0600

    cambios

--- resources/views/letter/dashboard/dashboard.blade.php
+++ resources/views/letter/dashboard/dashboard.blade.php
@@ -4,17 +4,25 @@
 
     <div class="main-panel">
         <div class="content-wrapper">
+
+
+
             <div class="row">
                 <div class="col-md-12 grid-margin">
-                    <div class="row">
+                    <div class="row align-items-center">
                         <div class="col-12 col-xl-8 mb-4 mb-xl-0">
                             <h3 class="font-weight-bold">Gestión de control</h3>
                             <h5 class="font-weight-normal mb-0">Dashboard</h5>
                         </div>
+                        <div class="col-12 col-xl-4 text-xl-right">
+                            <button style="background:black" type="button" class="btn btn-dark btn-icon-text">
+                                Reporte
+                                <i class="ti-layout btn-icon-append"></i>
+                            </button>
+                        </div>
                     </div>
                 </div>
             </div>
-
 
             <div class="row">
                 <div class="col-md-12 grid-margin stretch-card">
