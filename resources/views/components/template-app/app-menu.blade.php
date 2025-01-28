661f6d582ec45c2a6ef07bd7749bd228cf03ef54
Aldair Trejo <147780739+aldairtrejot@users.noreply.github.com>
2025-01-28 11:16:38 -0600

    cambios en menu

--- resources/views/components/template-app/app-menu.blade.php
+++ resources/views/components/template-app/app-menu.blade.php
@@ -29,31 +29,32 @@
 
         <!-- Item Correspondencia -->
         @if($letterMatch)
-            <li class="nav-item">
-                <a class="nav-link" data-toggle="collapse" href="#ui-basic_corres" aria-expanded="false"
-                    aria-controls="ui-basic_corres">
-                    <i class="fa fa-file-text menu-icon"></i>
-                    <span class="menu-title">G. Control</span>
-                    <i class="menu-arrow"></i>
-                </a>
-                <div class="collapse" id="ui-basic_corres">
-                    <ul class="nav flex-column sub-menu">
-                        <!--
-                                                                <li class="nav-item"><a class="nav-link" href="#">Administración</a></li>
-                                        -->
-                        <li class="nav-item"><a class="nav-link" href="{{ route('letter.list') }}">Correspondencia</a></li>
-                        <li class="nav-item"><a class="nav-link" href="{{ route('office.list') }}">Oficios</a></li>
-                        @if($letterAdminMatch)
-                            <li class="nav-item"><a class="nav-link" href="{{ route('inside.list') }}">Interno</a></li>
-                            <li class="nav-item"><a class="nav-link" href="{{ route('round.list') }}">Circulares</a></li>
-                            <li class="nav-item"><a class="nav-link" href="{{ route(name: 'file.list') }}">Lineamientos</a></li>
+                    <li class="nav-item">
+                        <a class="nav-link" data-toggle="collapse" href="#ui-basic_corres" aria-expanded="false"
+                            aria-controls="ui-basic_corres">
+                            <i class="fa fa-file-text menu-icon"></i>
+                            <span class="menu-title">G. Control</span>
+                            <i class="menu-arrow"></i>
+                        </a>
+                        <div class="collapse" id="ui-basic_corres">
+                            <ul class="nav flex-column sub-menu">
+                                <!--
+                                                                            <li class="nav-item"><a class="nav-link" href="#">Administración</a></li>
+                                                    -->
+                                <li class="nav-item"><a class="nav-link" href="{{ route('letter.list') }}">Correspondencia</a></li>
+                                <li class="nav-item"><a class="nav-link" href="{{ route('office.list') }}">Oficios</a></li>
+                                @if($letterAdminMatch)
+                                                        <!--
+                                                                <li class="nav-item"><a class="nav-link" href="{{ route('inside.list') }}">Interno</a></li>
+                                                                <li class="nav-item"><a class="nav-link" href="{{ route('round.list') }}">Circulares</a></li>
+                                                                <li class="nav-item"><a class="nav-link" href="{{ route(name: 'file.list') }}">Lineamientos</a></li>
+                                    -->
 
+                                @endif
 
-                        @endif
-
-                    </ul>
-                </div>
-            </li>
+                            </ul>
+                        </div>
+                    </li>
         @endif
 
         <!-- Item Cursos -->
