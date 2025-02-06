28dc7e06a1a686c5a4e157d5a5ae40a02b11cd16
Aldair Trejo <147780739+aldairtrejot@users.noreply.github.com>
2025-02-06 09:35:24 -0600

    cambios en mayusuculas

--- app/Http/Controllers/Letter/Inside/InsideC.php
+++ app/Http/Controllers/Letter/Inside/InsideC.php
@@ -179,7 +179,7 @@ class InsideC extends Controller
                 'id_usuario_area' => $request->id_usuario_area,
                 'id_usuario_enlace' => $request->id_usuario_enlace,
                 'id_cat_area' => $request->id_cat_area_documento,
-                'destinatario' => $request->destinatario,
+                'destinatario' => strtoupper($request->destinatario),
 
                 // DATA_SYSTEM
                 'id_usuario_sistema' => Auth::user()->id,
@@ -212,7 +212,7 @@ class InsideC extends Controller
                 'id_cat_area' => $request->id_cat_area_documento,
                 'id_usuario_sistema' => Auth::user()->id,
                 'fecha_usuario' => $now,
-                'destinatario' => $request->destinatario,
+                'destinatario' => strtoupper($request->destinatario),
             ];
 
             $object::where('id_tbl_interno', $request->id_tbl_interno)
