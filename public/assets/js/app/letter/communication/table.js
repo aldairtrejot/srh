75467746c390df3f9012e3200d792b3cded14ac7
Aldair Trejo <147780739+aldairtrejot@users.noreply.github.com>
2025-02-10 14:56:48 -0600

    mas cambios

--- public/assets/js/app/letter/communication/table.js
+++ public/assets/js/app/letter/communication/table.js
@@ -55,7 +55,7 @@ function searchInit() {
                         <td>${object.fecha}</td>
                         <td class="button-column">
                             ${object.uuid_oficio == null ? `
-                                <button onclick="download('${object.id}')" style="background:#003366" class="custom-button centered-button" title="Cargar">
+                                <button  style="background:#003366" class="custom-button centered-button" title="Cargar">
                                         <i style="color: white; font-size: 15px" class="fas fa-upload"></i>
                                     </button>
                             ` : `
