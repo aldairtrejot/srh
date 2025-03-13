a01e87aa26e160ea22a3db860b911ddbb3402000
Gustavo Ortiz <gustavo.ortiz@imssbienestar.gob.mx>
2025-03-12 18:02:51 -0600

    guardar cambios

--- app/Models/Courses/Courses/TblcoursesM.php
+++ app/Models/Courses/Courses/TblcoursesM.php
@@ -7,7 +7,7 @@ use Illuminate\Support\Facades\DB;
 
 class TblcoursesM extends Model
 {
-    protected $table = 'administration.users';
+    protected $table = 'capacitacion.tbl_cursos';
     protected $primaryKey = 'id_tbl_cursos';
     public $timestamps = false;
 
