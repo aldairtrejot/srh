fac453c927a8544cf7f6a8b7115b354506ff3173
Aldair Trejo <147780739+aldairtrejot@users.noreply.github.com>
2025-03-20 12:08:24 -0600

    mejoras en seguridad de login

--- app/Http/Controllers/Administration/LoginC.php
+++ app/Http/Controllers/Administration/LoginC.php
@@ -1,15 +1,16 @@
 <?php
 
 namespace App\Http\Controllers\Administration;
-
+use Illuminate\Support\Facades\Log;
 use App\Http\Controllers\Controller;
 use App\Models\Administration\LoginM;
-use Auth;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\RateLimiter;
 use Redirect;
-use Illuminate\Support\Facades\DB;
+use Auth;
 class LoginC extends Controller
 {
+    // Retorna la vista inicial de login
     public function __invoke()
     {
         return view('administration/login');
@@ -20,6 +21,9 @@ class LoginC extends Controller
         // Crear objeto del modelo
         $loginM = new LoginM();
 
+        // Data
+        $key = 'login-attempts:' . $request->ip();
+
         //  Primero validar los datos (incluyendo el CAPTCHA)
         $request->validate([
             'email' => 'required|email',
@@ -27,11 +31,26 @@ class LoginC extends Controller
             //'captcha' => 'required|captcha' //  Validar solo aquí, no en Auth::attempt()
         ]);
 
+        // Validación de inicio de sesion para max 10 por minuto
+        if (RateLimiter::tooManyAttempts($key, 10)) {
+            return back()->with([
+                'value' => 'error', // VALUE_IS(error, warning, success)
+                'message' => 'Demasiados intentos. Intenta en 1 minuto.',
+                'estatus' => 'true'
+            ]);
+        }
+
         //  Intentar autenticar al usuario (sin incluir el CAPTCHA)
         $credentials = $request->only('email', 'password'); //  Excluir 'captcha'
 
         if (Auth::attempt($credentials)) {
             // Si la autenticación es exitosa, regeneramos la sesión y redirigimos al dashboard
+            Log::info('LOG_LOGIN_RECORD: ', [
+                'id_user' => Auth::id(),
+                'ip_user' => $request->ip(),
+                'time_user' => now(),
+            ]);// Log de inicio de sesion
+
             $request->session()->regenerate();
             $userId = Auth::id();
             $roleUser = $loginM->validate($userId);
@@ -40,6 +59,7 @@ class LoginC extends Controller
         }
 
         // Redirección a login con mensaje de error
+        RateLimiter::hit($key, 60); // Expira en 60 segundos
         return back()->with([
             'value' => 'error', // VALUE_IS(error, warning, success)
             'message' => 'Información de inicio de sesión incorrecta.',
@@ -47,6 +67,7 @@ class LoginC extends Controller
         ]);
     }
 
+    // Cierre de sesión
     public function logout(Request $request, Redirect $redirect)
     {
         Auth::logout();
