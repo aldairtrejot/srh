1d6675e7963cb66521829a6ab7e0d202f5885263
Gustavo Ortiz <gustavo.ortiz@imssbienestar.gob.mx>
2025-04-03 18:55:52 -0600

    guardar cambios

--- public/assets/js/app/letter/dependencia/table.js
+++ public/assets/js/app/letter/dependencia/table.js
@@ -62,7 +62,7 @@ function searchInit() {
 
             if (response.value && response.value.length > 0) {
                 response.value.forEach(function (object) {
-                    const finalUrl = `${URL_DEFAULT}/dependencia/edit/${object.id}`;
+                    const finalUrl = `${URL_DEFAULT}/dependencia/edit/${object.id_cat_dependencia}`;
                     const estatusTexto = object.estatus ? 'ACTIVO' : 'INACTIVO';
                     const estatusColor = estatusColors[estatusTexto] || '#999';
 
