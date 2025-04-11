f5d1f3707f3f9843be98fcd89d2bc54d116cf31d
Gustavo Ortiz <gustavo.ortiz@imssbienestar.gob.mx>
2025-04-11 14:21:34 -0600

    guardar cambios
    catalogo cloud guardado con la alerta

--- resources/views/administration/configcloudC/form.blade.php
+++ resources/views/administration/configcloudC/form.blade.php
@@ -8,51 +8,57 @@
                     <div class="row">
                         <x-template-tittle.tittle-header tittle="Config cloud" caption="Cloud" />
                     </div>
+                    <br>
+                    <br>
+
                 </div>
-            </div>
 
-            <div class="col-lg-12 grid-margin stretch-card">
-                <div class="card custom-card">
-                    <div class="card-body">
-                        <x-template-tittle.tittle-caption
-                            tittle="{{ isset($item->id_config_cloud) ? 'Modificar' : 'Agregar' }} Cloud"
-                            route="{{ route('configcloud.list') }}" />
-                        
-                        <br>
+                <div class="col-lg-12 grid-margin stretch-card">
+                    <div class="card custom-card">
+                        <div class="card-body">
+                            <x-template-tittle.tittle-caption
+                                tittle="{{ isset($item->id_config_cloud) ? 'Modificar' : 'Agregar' }} Cloud"
+                                route="{{ route('configcloud.list') }}" />
+                            <br>
+                            <div class="alert alert-warning d-flex align-items-center" role="alert" style="font-size: 1rem; color: black;">
+                                <i class="fa-solid fa-triangle-exclamation me-2" style="color: #c29306;"></i>
+                                <div>
+                                    <strong>Advertencia:</strong> Modificar estas configuraciones puede afectar el funcionamiento del sistema con los servicios en la nube.
+                                </div>
+                            </div>
+                            <br>
+                            <form action="{{ route('configcloud.save') }}" method="POST" class="form-sample"
+                                id="myForm">
+                                @csrf
 
-                        <form action="{{ route('configcloud.save') }}" method="POST" class="form-sample" id="myForm">
-                            @csrf
-
-                            <x-template-form.template-form-input-hidden name="id_config_cloud"
-                                value="{{ optional($item)->id_config_cloud ?? '' }}" />
+                                <x-template-form.template-form-input-hidden name="id_config_cloud"
+                                    value="{{ optional($item)->id_config_cloud ?? '' }}" />
 
                                 <x-template-form.template-form-input-required label="Nombre" type="text"
-                                name="nombre" placeholder="Nombre"
-                                grid="col-8 col-sm-8 col-md-8 col-lg-8 col-xl-8" autocomplete=""
-                                value="{{ optional($item)->nombre ?? '' }}" />
+                                    name="nombre" placeholder="Nombre" grid="col-8 col-sm-8 col-md-8 col-lg-8 col-xl-8"
+                                    autocomplete="" value="{{ optional($item)->nombre ?? '' }}" />
 
-                            <x-template-form.template-form-input-required label="Descripción" type="text"
-                                name="descripcion" placeholder="Descripción"
-                                grid="col-8 col-sm-8 col-md-8 col-lg-8 col-xl-8" autocomplete=""
-                                value="{{ optional($item)->descripcion ?? '' }}" />
+                                <x-template-form.template-form-input-required label="Descripción" type="text"
+                                    name="descripcion" placeholder="Descripción"
+                                    grid="col-8 col-sm-8 col-md-8 col-lg-8 col-xl-8" autocomplete=""
+                                    value="{{ optional($item)->descripcion ?? '' }}" />
 
-                            <x-template-form.template-form-input-required label="Valor" type="text"
-                                name="valor" placeholder="Valor"
-                                grid="col-4 col-sm-4 col-md-4 col-lg-4 col-xl-4" autocomplete=""
-                                value="{{ optional($item)->valor ?? '' }}" />
+                                <x-template-form.template-form-input-required label="Valor" type="text"
+                                    name="valor" placeholder="Valor" grid="col-4 col-sm-4 col-md-4 col-lg-4 col-xl-4"
+                                    autocomplete="" value="{{ optional($item)->valor ?? '' }}" />
 
-                            <div class="col-4 col-sm-4 col-md-4 col-lg-4 col-xl-4">
-                                <label for="estatus">Estatus</label>
-                                <input type="checkbox" id="estatus" name="estatus" class="toggle-switch" checked>
-                            </div>
+                                <div class="col-4 col-sm-4 col-md-4 col-lg-4 col-xl-4">
+                                    <label for="estatus">Estatus</label>
+                                    <input type="checkbox" id="estatus" name="estatus" class="toggle-switch" checked>
+                                </div>
 
-                            <x-template-button.button-form-footer routeBack="{{ route('configcloud.list') }}" />
-                        </form>
+                                <x-template-button.button-form-footer routeBack="{{ route('configcloud.list') }}" />
+                            </form>
+                        </div>
                     </div>
                 </div>
             </div>
         </div>
-    </div>
 
-    
+
 </x-template-app.app-layout>
