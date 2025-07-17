ac6c233ff998953b84ce194dfec164522f88e30b
Gustavo Ortiz <gustavo.ortiz@imssbienestar.gob.mx>
2025-07-16 18:42:59 -0600

    guardar
    volver a sacar el insertar en mi tabla

--- app/Http/Controllers/Files/Files/FileschecklistC.php
+++ app/Http/Controllers/Files/Files/FileschecklistC.php
@@ -4,7 +4,6 @@ namespace App\Http\Controllers\Files\Files;
 
 use App\Http\Controllers\Controller;
 use App\Models\Files\Files\FileschecklistM;
-use App\Models\Files\Files\FilesdocumentM;
 use Illuminate\Http\Request;
 use Carbon\Carbon;
 use Illuminate\Support\Facades\Auth;
