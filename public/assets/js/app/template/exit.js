d43cb96437e35a427cd765b1891ae42458383fbe
Aldair Trejo <147780739+aldairtrejot@users.noreply.github.com>
2025-10-09 12:28:13 -0600

    cambios finales

--- public/assets/js/app/template/exit.js
+++ public/assets/js/app/template/exit.js
@@ -1,12 +1,11 @@
 document.addEventListener("DOMContentLoaded", function () {
     // Valor de Laravel (.env), en minutos
-    const sessionLifetime = 1; //window.SESSION_LIFETIME - 2;
+    const sessionLifetime = window.SESSION_LIFETIME - 2;
     const sessionMs = sessionLifetime * 60 * 1000; // convertir a ms
 
     // Contador regresivo
     setTimeout(() => {
         // Aquí abres el modal cuando el tiempo expira
-        console.log('logout...')
         var token = $('meta[name="csrf-token"]').attr('content');
         $.ajax({
             url: URL_DEFAULT.concat('/session-lifetime-logout'),
@@ -15,7 +14,6 @@ document.addEventListener("DOMContentLoaded", function () {
                 _token: token  // Usar el token extraído de la metaetiqueta
             },
             success: function (response) {
-                console.log(response)
                 if (response.status) {
                     window.location.href = URL_DEFAULT.concat('/login');
                 } else {
